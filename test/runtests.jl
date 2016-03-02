fc340aa4b84cd76cbbafad5028184ff4a152a602
Gerhard Tulzer <gerhard.tulzer@tuwien.ac.at>
2016-03-02 11:24:02 +0100

    Copied code to respective files.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,12 @@
 using EllipticFEM
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+meshpath="mesh_p05.msh";
+A = 1.0;
+f = (x,y)->5/4*pi^2*cos(x*pi/2)*sin(y*pi);
+bddata = [1 2 3 4;
+		'D' 'N' 'D' 'N';
+		(x,y)->0 (x,y)->-pi/2*sin(x*pi/2)*sin(y*pi) (x,y)->0 (x,y)->pi/2*sin(x*pi/2)*sin(y	*pi)];
+mesh,u = solve(meshpath,A,f,bddata);
+
+plot_solution(mesh,u)
